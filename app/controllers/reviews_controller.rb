769dccea6cfc19bce1a59e02aa4af4cb7e4477e0
blackwell <blackwellsmithiv@gmail.com>
2019-04-30 23:21:04 +0000

    many changes added show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,11 @@
 class ReviewsController < ApplicationController
   
+  get '/index' do
+     @reviews = Review.all
+     erb :'/reviews/index'
+   end
+   
+  
   get '/reviews' do
     erb :'/reviews/home'
   end
@@ -7,4 +13,14 @@ class ReviewsController < ApplicationController
   get '/create' do
     erb :'/reviews/create'
   end
+  
+  post '/reviews' do
+     @review = Review.create(params)
+     redirect to "/reviews/#{@review.id}"
+   end
+   
+    get '/reviews/:id' do
+      @review = Review.find(params[:id])
+     erb :'reviews/show'
+   end
 end
