75c8e7863787d637948b5b37e37d3e342d6734c6
blackwell <blackwellsmithiv@gmail.com>
2019-04-30 19:12:08 +0000

    created sign up view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  
+   post '/signup' do 
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.create(:username => params[:username], :password => params[:password])
+      session[:user_id] = @user.id
+      redirect '/?'
+    end
+  end
+  
+end
