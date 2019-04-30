57af37d103b7d2cf2545069f7edcad48978895c7
blackwell <blackwellsmithiv@gmail.com>
2019-04-30 20:46:36 +0000

    added to contoller and created login view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,30 @@
 class UsersController < ApplicationController
   
+   get '/signup' do
+      erb :'users/new'
+  end
+  
    post '/signup' do 
     if params[:username] == "" || params[:password] == ""
       redirect to '/signup'
     else
       @user = User.create(:username => params[:username], :password => params[:password])
       session[:user_id] = @user.id
-      redirect '/?'
+      redirect to '/login'
+    end
+  end
+  
+  get '/login' do
+    erb :'users/login'
+  end 
+  
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/index"
+    else
+      redirect to '/signup'
     end
   end
   
