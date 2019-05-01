79aeaddfd9dd010d47e39f5536e48ab97e06e682
blackwell <blackwellsmithiv@gmail.com>
2019-05-01 09:52:09 +0000

    added patch and started delete function in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -23,4 +23,26 @@ class ReviewsController < ApplicationController
       @review = Review.find(params[:id])
      erb :'reviews/show'
    end
+   
+    get '/reviews/:id/edit' do 
+     @review = Review.find(params[:id])
+     erb :'/reviews/edit'
+   end
+   
+    patch '/reviews/:id' do
+     @review = Review.find(params[:id])
+     @review.name = params[:name]
+     @review.content = params[:content]
+     @review.save
+     redirect to "/reviews/#{@review.id}"
+     
+   end
+   
+    delete '/recipes/:id' do
+      recipe = Recipe.find(params[:id])
+      recipe.delete
+      #binding.pry
+      redirect to '/recipes'
+
+  end
 end
